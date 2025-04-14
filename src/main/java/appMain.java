023d256f7e7bc6a7a814c2ce456f2fbae9843084
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-14 18:41:13 +0200

    commit initial menu

---
+++ src/main/java/appMain.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class appMain {
+    public static void main(String[] args) {
+
+
+        // Menu Principal
+
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Elige");
+            choice = scanner.nextInt();
+
+            System.out.println("Hei");
+        } while (choice != 0);
+
+
+    }
+}
