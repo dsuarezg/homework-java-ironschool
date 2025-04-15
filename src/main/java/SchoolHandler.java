5b58f831207e9cc51f58317ff5d43315c9092fd3
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-04-15 16:29:42 +0200

    SchoolHandler

--- src/main/java/SchoolHandler.java
+++ src/main/java/SchoolHandler.java
@@ -8,18 +8,19 @@ public class SchoolHandler {
     static HashMap<String,Course> courses = new HashMap<>();
     static HashMap<String,Student> students = new HashMap<>();
 
-    // create School
-    public static void createSchool(){
+    // create School Course and Student
+    public static void createSchool() throws IllegalArgumentException{
         System.out.println("Introduce l nombre de la escuela");
         schoolName=scanner.nextLine();
+        // se debe de guardar schoolName
         System.out.println("indica el numero de profesores");
-        int numberTeacher=scanner.nextInt();
+        int numberTeacher=Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < numberTeacher; i++) {
             Teacher teacher=createTeacher();
             teachers.put(teacher.getTeacherId(),teacher);
         }
         System.out.println("Introduce el numero de cursos que quieres añadir");
-        int numberOfCourses=scanner.nextInt();
+        int numberOfCourses=Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < numberOfCourses; i++) {
             Course course=createCourse();
             courses.put(course.getCourseId(), course);
@@ -34,5 +35,32 @@ public class SchoolHandler {
 
 
 
+    }
+
+    private static Student createStudent() {
+        System.out.println("Name of the Student");
+        String name=scanner.nextLine();
+        System.out.println("Address of the Student");
+        String address=scanner.nextLine();
+        System.out.println("Email of the Student");
+        String email=scanner.nextLine();
+        return new Student(name,address,email);
+    }
+
+    public static Course createCourse() {
+        System.out.println("Name of the Course");
+        String name=scanner.nextLine();
+        System.out.println("Price of the Course");
+        Double salary=Double.parseDouble(scanner.nextLine());
+        return new Course(name,salary);
+    }
+
+    public static Teacher createTeacher() {
+        //
+        System.out.println("Name of the Teacher");
+        String name=scanner.nextLine();
+        System.out.println("salary of the Teacher");
+        Double salary=Double.parseDouble(scanner.nextLine());
+        return new Teacher(name,salary);
     }
 }
--- src/main/java/appMain.java
+++ src/main/java/appMain.java
@@ -5,42 +5,52 @@ public class appMain {
     public static void main(String[] args) {
         // Menu Principal
 
-        HashMap<String,Teacher> teachers = new HashMap<>();
-        HashMap<String,Course> courses = new HashMap<>();
-        HashMap<String,Student> students = new HashMap<>();
+
 
         Scanner scanner = new Scanner(System.in);
-        String[] optionSuperMenu={"Program Start","Submenu","Comands"};
-        String[] optionsMenu={"Nombre de la Escuela","numero de Profesores",
-                "numero de cursos","detalles del curso","salir"};
+        String[] optionSuperMenu={"Load data manually ",
+                "Load data from Json","Exit"};
+        String[] optionsMenu={"Entry a new Teacher",
+                "Entry a Course","Entry a Student","salir"};
         int userchoice=0;
 
 
-        while (userchoice!=5){
+        while (true){
             System.out.println("Elige una opcion");
-            for (int i = 0; i < optionSuperMenu.length; i++) {
+            for (int i = 0; i <optionSuperMenu.length ; i++) {
                 System.out.println((i+1)+" "+optionSuperMenu[i]);
             }
             // eleccion del usuario
             userchoice = scanner.nextInt();
-            System.out.println(userchoice);
+
+
             if (userchoice<1||userchoice>5){
                 System.out.println("Debes introducir una opcion valida");
             }
-//            switch (userchoice){
-//                case 1:
-//                    // return boolean
-//                    createSchool();
-//                case 2:
-//                    //return boolean
-//                    createTeacher();
-//                case 3:
-//                    //return boolean
-//                    //createCourses();
-//                case 4:
-//            }
+            switch (userchoice){
+                case 1:
+                    // return boolean
+                    try {
+                    SchoolHandler.createSchool();
+                    }catch (Exception e){
+                        //empty maps
+                        SchoolHandler.teachers.clear();
+                        SchoolHandler.students.clear();
+                        SchoolHandler.courses.clear();
+                        System.out.println("School creation failed");
+                    }
+
+                case 2:
+                    //return boolean
+                case 3:
+                    //return boolean
+                    //createCourses();
+                case 4:
+                    userchoice=5;
+                    break;
+            }
         }
 
-        System.out.println("salio");
+
     }
 }
